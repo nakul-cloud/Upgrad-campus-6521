0a922beb9b0fbfa97b6b4cb542184869f2092ed2
Nakul Jadhav <jadhavnakul456@gmail.com>
2023-10-21 20:38:40 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+        int number = -5;
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is negative.");
+
+        }
+    }
+}
