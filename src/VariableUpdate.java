2530f04f2a88f1254e17c89b0ff7fd7ba0adf332
Nakul Jadhav <jadhavnakul456@gmail.com>
2023-10-22 13:00:01 +0530

    first commit

---
+++ src/VariableUpdate.java
@@ -0,0 +1,16 @@
+public class VariableUpdate{
+    public static void main(String[] args) {
+        int marksJose = 0,  marksJuan = 0;
+
+        System.out.println(marksJose);
+        System.out.println(marksJuan);
+
+        marksJose = marksJose + 50;
+        marksJuan = 80;
+
+        System.out.println(marksJose);
+        System.out.println(marksJuan);
+
+    }
+
+}
