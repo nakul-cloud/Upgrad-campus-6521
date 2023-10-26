70e1ddbe132ff04a196d69ef28d76b12834ff291
Nakul Jadhav <jadhavnakul456@gmail.com>
2023-10-26 20:01:44 +0530

    first commit

---
+++ src/casting.java
@@ -0,0 +1,9 @@
+public class casting {
+    public static void main(String[] args) {
+        int distanceInMetres = 2367;
+        float distance = 2.367f;
+
+        double average = ((double)distanceInMetres +(double)distance)/2;
+        System.out.println(average);
+    }
+}
